d856def24c30d934c5803986bf85d4afa74f8be5
LouiMinister <louiminister@gmail.com>
2022-07-06 16:23:12 +0900

    스프링 핵심 원리 - 기본편 - Spring - 컴포넌트 스캔

---
+++ 02.spring-core-basic/core/src/test/java/hello/core/filter/MyExcludeComponent.java
@@ -0,0 +1,10 @@
+package hello.core.filter;
+import org.springframework.stereotype.Indexed;
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Indexed
+public @interface MyExcludeComponent {
+}
